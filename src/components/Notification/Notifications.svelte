db6355dc22017ced3d5eb9dfc5579915383ea792
Felipe Vilemondes <45976599+akenoduch@users.noreply.github.com>
2023-08-10 13:59:25 -0300

    feat. Suggest Water Amount on Notification

--- src/components/Notification/Notifications.svelte
+++ src/components/Notification/Notifications.svelte
@@ -3,7 +3,6 @@
   import { onMount } from "svelte";
 
   onMount(async () => {
-    console.log("onMount called");
     // Solicitar permissões para notificações locais
     await LocalNotifications.requestPermissions();
 
@@ -12,21 +11,15 @@
     if (display === "granted") {
       scheduleNotifications();
     } else {
-      scheduleNotifications(); // Mesmo se não concedido, agendamos as notificações (ajuste conforme necessário)
+      scheduleNotifications();
     }
-    console.log("Permission status:", display);
   });
 
   function scheduleNotifications() {
-    console.log("Scheduling notifications...");
-    // Intervalo de 30 minutos para a notificação de pausa
-    const breakInterval = 30 * 60 * 1000;
     // Intervalo de 15 minutos para a notificação de água
     const waterInterval = 15 * 60 * 1000;
 
-    // Loop para agendar notificações
     setInterval(() => {
-      console.log("Interval called");
       // Obter o horário atual
       const now = new Date();
       const currentTime =
@@ -36,36 +29,30 @@
       const wakeUpTime = localStorage.getItem("wakeUpTime").replace(/"/g, "");
       const sleepTime = localStorage.getItem("sleepTime").replace(/"/g, "");
 
-      console.log("Current Time:", currentTime);
-      console.log("Wake Up Time:", wakeUpTime);
-      console.log("Sleep Time:", sleepTime);
+      // Obter remainingWaterIntake do Local Storage
+      const waterIntake = Number(localStorage.getItem("waterIntake") || 0);
+      const currentWaterConsumed = getWaterDataForToday().currentWaterConsumed;
+      const remainingWaterIntake = waterIntake - currentWaterConsumed;
 
-      // Verificar se o horário atual está dentro do intervalo
-      if (isTimeInRange(currentTime, wakeUpTime, sleepTime)) {
-        // Agendar notificação de pausa
-        console.log("Scheduling break notification for 30 minutes from now.");
-        LocalNotifications.schedule({
-          notifications: [
-            {
-              title: "Break Time",
-              body: "Take a break",
-              id: now.getTime(),
-              schedule: { at: new Date(Date.now() + breakInterval) },
-              sound: null,
-              attachments: null,
-              actionTypeId: "",
-              extra: null,
-            },
-          ],
-        });
+      if (
+        isTimeInRange(currentTime, wakeUpTime, sleepTime) &&
+        remainingWaterIntake > 0
+      ) {
+        // Calcular o número de notificações restantes
+        const notificationsRemaining =
+          (timeToMinutes(sleepTime) - timeToMinutes(currentTime)) /
+          (waterInterval / (60 * 1000));
+
+        // Calcular a quantidade sugerida de água para beber
+        const suggestedWaterAmount =
+          remainingWaterIntake / notificationsRemaining;
 
         // Agendar notificação de água
-        console.log("Scheduling water notification for 15 minutes from now.");
         LocalNotifications.schedule({
           notifications: [
             {
               title: "Water Time",
-              body: "Drink some water",
+              body: `Drink ${suggestedWaterAmount.toFixed(2)} ml of water`,
               id: now.getTime() + 1,
               schedule: { at: new Date(Date.now() + waterInterval) },
               sound: null,
@@ -75,30 +62,36 @@
             },
           ],
         });
-      } else {
-        console.log("Current time is not in the specified range.");
       }
-    }, Math.min(breakInterval, waterInterval)); // Usar o menor intervalo para o loop
+    }, waterInterval); // Usar o intervalo de água para o loop
   }
 
-  // Função para converter um horário em minutos
   function timeToMinutes(timeString) {
     const [hours, minutes] = timeString.split(":").map(Number);
     return hours * 60 + minutes;
   }
 
-  // Função para verificar se um horário está dentro de um intervalo
   function isTimeInRange(time, startTime, endTime) {
     const timeInMinutes = timeToMinutes(time);
     const startTimeInMinutes = timeToMinutes(startTime);
     const endTimeInMinutes = timeToMinutes(endTime);
-
-    console.log("Time in minutes:", timeInMinutes);
-    console.log("Start Time in minutes:", startTimeInMinutes);
-    console.log("End Time in minutes:", endTimeInMinutes);
-
     return (
       timeInMinutes >= startTimeInMinutes && timeInMinutes <= endTimeInMinutes
     );
+  }
+
+  function getWaterDataForToday() {
+    let waterDataArray =
+      JSON.parse(localStorage.getItem("waterDataArray")) || [];
+    return (
+      waterDataArray.find((item) => item.date === getToday()) || {
+        currentWaterConsumed: 0,
+      }
+    );
+  }
+
+  function getToday() {
+    const today = new Date();
+    return today.toISOString().split("T")[0];
   }
 </script>
